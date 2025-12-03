db1378f95a845587edad3f4ca85d4899208ac82d
Alexandre Komi Wottor <wottoralexandre@gmail.com>
2025-12-03 19:26:04 +0000

    Initial commit

---
+++ ManangerAP/src/main/java/com/ManangerAP/ManangerAP/ManangerApApplication.java
@@ -0,0 +1,13 @@
+package com.ManangerAP.ManangerAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManangerApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManangerApApplication.class, args);
+	}
+
+}
